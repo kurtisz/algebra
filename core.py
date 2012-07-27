70e2789fecaa783f93c99871931bb13c459e2640
Kurtis Zimmerman <zimmerka@rose-hulman.edu>
2012-07-27 15:23:47 -0400

    Group, Z, and U from ground up, using universal algebraic approach

---
+++ core.py
@@ -0,0 +1,111 @@
+from fractions import gcd
+from groupUtils import getInverse
+
+class Atom(object):
+    def __init__(self, value):
+        self._value = value
+    
+    def __str__(self):
+        return str(self._value)
+    
+    def eval(self):
+        return self._value
+    
+    def __hash__(self):
+        return hash(self._value)
+    
+    def __eq__(self, other):
+        if not type(other) == type(self):
+            return False
+        return self._value == other._value
+
+class Set(object):
+    def __init__(self, atoms):
+        self._elements = []
+        for a in atoms:
+            if not a in self._elements:
+                self._elements.append(a)
+    
+    def __contains__(self, atom):
+        return atom in self._elements
+    
+    def __iter__(self):
+        for a in self._elements:
+            yield a
+    
+    def __len__(self):
+        return len(self._elements)
+    
+    def __eq__(self, other):
+        if not type(other) == type(self):
+            return False
+        if not len(self) == len(other):
+            return False
+        for a in self._elements:
+            if not a in other:
+                return False
+        return True
+    
+    def __str__(self):
+        return str([str(a) for a in self._elements])
+
+class Operation(object):
+    '''
+        Represents an n-ary operation
+        for a given domain
+        
+        Domains must be sets of n-tuples
+        for the given n-ary operation
+    '''
+    
+    def __init__(self, domain, function):
+        self._domain = domain
+        self._arity =function.func_code.co_argcount
+        self._mapping = {x:applyFunction(function, x) for x in domain}
+    
+    def eval(self, x):
+        if x not in self._domain:
+            raise ValueError("{0} not in domain".format(x))
+        return self._mapping[x]
+    
+    def arity(self):
+        return self._arity
+    
+    def __len__(self):
+        return self.arity()
+
+class NullaryOperation(Operation):
+    def __init__(self, value):
+        super(NullaryOperation, self).__init__([()], lambda : value)
+    
+    def eval(self):
+        return super(NullaryOperation, self).eval(())
+
+class BinaryOperation(Operation):
+    def __init__(self, S, function):
+        super(BinaryOperation, self).__init__(reduce(lambda row1, row2: row1 + row2, [[(a, b) for b in S] for a in S]), function)
+
+def applyFunction(f, tup):
+    if type(tup) == type(1):
+        return f(tup)
+    if len(tup) == 0:
+        return f()
+    return f(*tup)
+
+class Magma(object):
+    '''
+        A set closed over a binary operation
+    '''
+    
+    def __init__(self, elements, function):
+        self._set = elements
+        self._operation = BinaryOperation(elements, function)
+    
+    def add(self, a, b):
+        if (not a in self._set) or (not b in self._set):
+            raise ValueError("({0}, {1}) not in domain".format(a, b))
+        return self._operation.eval((a,b))
+    
+    def __iter__(self):
+        for a in self._set:
+            yield a
--- group.py
+++ group.py
@@ -1,82 +1,55 @@
 from fractions import gcd
-from groupUtils import getIdentity, getInverse, getOrder, commutes
+from groupUtils import getInverse
+from core import Magma, NullaryOperation, Operation
 
-class Element(object):
-    def __init__(self, value, op):
-        self._value = value
-        self._op = op
-        self._inverse = None
-        self._order = 0
-        
-    def __add__(self, other):
-        return self._op(self._value, other._value)
-    
-    def __str__(self):
-        return str(self._value)
-    
-    def __eq__(self, other):
-        return self._value == other._value
-    
-    def __neg__(self):
-        return self._inverse
-    
-    def __len__(self):
-        return self._order
-    
-    def generateCyclicSubgroup(self):
-        # returns a list of elements in subgroup
-        H = []
-        cum = a
-        for i in range(self._order):
-            H.append(cum)
-            cum = cum + a
-        return H
-
-class Group(object):        
-    def __init__(self, elements, operation):
-        self._operation = lambda x, y: self._elements[operation(x,y)]
-        self._identity = getIdentity(elements, operation)
-        self._elements = {e:Element(e, self._operation) for e in elements}
-        for e in self:
-            e._inverse = self._elements[getInverse(e._value, elements, operation, self._identity)]
-            e._order = getOrder(e._value, operation, self._identity, len(elements))
-    
-    def __len__(self):
-        # the "order" of the group
-        return len(self._elements)
-    
-    def __iter__(self):
-        for key in sorted(self._elements.keys()):
-            yield self._elements[key]
-    
-    def __contains__(self, a):
-        return a in self._elements.values()
-    
-    def __getitem__(self, a):
-        return self._elements[a]
+class Loop(Magma):
+    def __init__(self, elements, function, identity):
+        super(Loop, self).__init__(elements, function)
+        self._e = NullaryOperation(identity)
     
     def identity(self):
-        return self._elements[self._identity]
+        return self._e.eval()
+
+class Group(Loop):
+    def __init__(self, elements, function, inverseFunction, identity):
+        super(Group, self).__init__(elements, function, identity)
+        self._inverse = Operation([(x) for x in elements], inverseFunction)
+        self._order = len(elements)
     
-    def center(self):
-        c = []
-        for a in self:
-            if commutes(a, self):
-                c.append(a)
-        return c
+    def inverse(self, x):
+        return self._inverse.eval((x))
 
 class Z(Group):
     def __init__(self, n):
-        super(Z, self).__init__(list(range(n)), lambda x, y : (x + y) % n)
+        super(Z, self).__init__(range(n), lambda x, y : (x + y) % n, lambda x : (n - x) % n, 0)
+    
+    def __str__(self):
+        return "Z_{0}".format(self._order)
 
 class U(Group):
     def __init__(self, n):
-        super(U, self).__init__([i for i in range(1,n) if gcd(i,n) == 1], lambda x, y : (x * y) % n)
+        S = [i for i in range(1,n) if gcd(i,n) == 1]
+        f = lambda x, y : (x * y) % n
+        self._n = n
+        super(U, self).__init__(S, f, lambda x : getInverse(x, S, f, 1), 1)
+    
+    def __str__(self):
+        return "U_{0}".format(self._n)
 
 if __name__ == '__main__':
-    Z5 = Z(8)
-    print len(Z5)
-    print [str(i) for i in Z5.center()]
-    for a in Z5:
-        print a, -a
-        print [str(i) for i in a.generateCyclicSubgroup()]
+    G = U(8)
+    print "Group:", G
+    print "\tIdentity:", G.identity()
+    print "\tInverses:"
+    for a in G:
+        print "\t\t{0}^(-1) = {1}".format(a, G.inverse(a))
+    print "\tCayley table:"
+    print "\t\t",
+    for a in G:
+        print "{0}\t".format(a),
+    print
+    for a in G:
+        print "\t{0}\t".format(a),
+        for b in G:
+            print "{0}\t".format(G.add(a,b)),
+        print
